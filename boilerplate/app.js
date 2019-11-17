1e2d224b3d855bdcbae15dbdc8b4f41a806d142e
Mohsin Ali <mali9299@gmail.com>
2019-11-17 11:52:10 -0500

    init commit

---
+++ boilerplate/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const employeeRoutes = require('./routes/employee');
+const app = express();
+const port = parseInt(process.env.PORT || '3000');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/employees', employeeRoutes);
+
+// Fail over route
+app.use(function(req, res) {
+    res.status(404).send('Not found');
+});
+
+// listen for requests
+app.listen(port, function() {
+    console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
