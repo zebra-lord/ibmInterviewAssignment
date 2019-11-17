3c8d8e83417366eb0b31a413ba6960aadd15fb75
Mohsin Ali <mali9299@gmail.com>
2019-11-17 17:02:08 -0500

    added sample entry to db

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -3,10 +3,21 @@
 const express = require('express');
 const router = express.Router();
 
-const DATABASE = {};
+const CEO_ID = null;
+
+const DATABASE = {
+  '1': {
+    'firstName': 'mohsin',
+    'lastName': 'ali',
+    'hireDate': '2019-11-02',
+    'role': 'LACKEY',
+    'quote1': '[On bowling] Straight down the middle. No hook, no spin, no fuss. Anything more and this becomes figure skating.',
+    'quote2': 'At that point where you have decided to upgrade from aspiration to expectation and have begun to visualize an outcome, something incredibly important has happened, you have committed to the process of change.'
+  }
+};
 
 /* GET employees listing. */
-router.get('', function(req, res) {
+router.get('/', (req, res) => {
   return res.send(DATABASE);
 });
 
