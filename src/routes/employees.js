41050211a23547d8bd6def016a75ab510807ef63
Mohsin Ali <mali9299@gmail.com>
2019-11-18 19:55:54 -0500

    Implements put api

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -84,7 +84,7 @@ const jokeRequester = axios.create({
  * Middleware to populate employee object with a favorite quote and quote
  * retrieved from external apis
  **/
-var populateQuotes = function(req, res, next) {
+var populateFavoriteJokeAndQuotes = function(req, res, next) {
   axios.all([ronSwansonQuoteRequester.get(), jokeRequester.get()])
     .then(axios.spread(function (rsQuote, joke) {
       req.employee.favoriteQuote = rsQuote.data[0] + " - Ron Swanson";
@@ -96,6 +96,22 @@ var populateQuotes = function(req, res, next) {
     })
 };
 
+var validateFaveJokeAndQuote = function(req, res, next) {
+  if (req.body.favoriteJoke && typeof req.body.favoriteJoke === 'string') {
+    req.employee.favoriteJoke = req.body.favoriteJoke;
+  } else {
+    return res.status(400).send("Favorite joke not porvided or of incorrect format.");
+  }
+
+  if (req.body.favoriteQuote && typeof req.body.favoriteQuote === 'string') {
+    req.employee.favoriteQuote = req.body.favoriteQuote;
+  } else {
+    return res.status(400).send("Favorite quote not porvided or of incorrect format.");
+  }
+
+  next();
+};
+
 /* GET employees listing. */
 router.get('/', (req, res) => {
   return res.send(DATABASE);
@@ -105,7 +121,7 @@ router.get('/', (req, res) => {
  * POST an employee to the database.
  * Responds with the employee object along with their id if successful.
  */
-router.post('/', [validateEmployeeData, populateQuotes], (req, res) => {
+router.post('/', [validateEmployeeData, populateFavoriteJokeAndQuotes], (req, res) => {
   // TODO: logic in this function should be handled in separate database management module
 
   const id = uuid();
@@ -130,14 +146,14 @@ router.route('/:id')
       return res.status(404).send(`No employee found with id ${id}`);
   })
   /* get employee for given id */
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send(DATABASE[req.params['id']])
   })
   /**
    * Delete employee for given id from the database.
    * Responds with the deleted employee if successful.
    */
-  .delete(function(req, res) {
+  .delete((req, res) => {
     let id = req.params['id']
     let employee = DATABASE[id];
     delete DATABASE[id];
@@ -147,6 +163,37 @@ router.route('/:id')
       CEO_ID = null;
     }
     res.send(employee);
+  })
+  /**
+   * Update employee data.
+   * 
+   * The api currently requrires all employee data, including the favorite
+   * quote and joke, to be provided. This probably isn't the right approach
+   * as we'll likely just want to update a couple pieces of employee information
+   * at a time.
+   * 
+   * Responds with the old employee data if successful.
+   */
+  .put([validateEmployeeData, validateFaveJokeAndQuote], (req, res) => {
+    let id = req.params['id']
+    let oldEmployee = DATABASE[id];
+
+    // special logic if role is being updated to CEO
+    if (req.employee.role === 'CEO') {
+      if (!CEO_ID) {
+        // if there isn't already a CEO keep track of the id
+        CEO_ID = id;
+      } else if (CEO_ID !== id) {
+        return res.status(400).send(`Employee ${CEO_ID} is already the CEO. Cannot upgrade this employee to CEO.`);
+      }
+    } else if (oldEmployee.role === 'CEO') {
+      // if we're downgrading the CEO reset the CEO_ID variable
+      CEO_ID = null;
+    }
+    console.log(`emp updated to ${req.employee}`);
+
+    DATABASE[id] = req.employee;
+    res.send(oldEmployee);
   });
 
 module.exports = router;
